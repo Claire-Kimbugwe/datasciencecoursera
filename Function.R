2b192ccb431db3c9a3843d5941849f59aac7b0aa
Claire-Kimbugwe <31630227+Claire-Kimbugwe@users.noreply.github.com>
2017-09-10 14:05:09 -0700

    Update Function.R

--- Function.R
+++ Function.R
@@ -3,18 +3,19 @@
 #list of monitors. The function 'pollutantmean' takes three arguments: 'directory', 'pollutant', and 'id'. Given a 
 #vector monitor ID numbers, 'pollutantmean' reads that monitors' particulate matter data from the directory specified in 
 #the 'directory' argument and returns the mean of the pollutant across all of the monitors, ignoring any missing values
-#coded as NA. A prototype of the function is as follows
+#coded as NA.
 
 pollutantmean <- function(directory, pollutant,id = 1:332){
   #list of files
   dirFiles <- dir(directory, full.names = TRUE)
   #create an empty data frame
   df <- data.frame()
-  #iterrate through the monitor dheets (id)
+  #iterrate through the monitor heets (id)
   for (item in id){
     #read data into a data frame
     df <- rbind(df, read.csv(dirFiles[item]))
   }
-  Our_mean <-  mean(df[,pollutant], na.rm = TRUE)
+  #return the mean
+  return(Our_mean <-  mean(df[,pollutant], na.rm = TRUE))
   
 }
