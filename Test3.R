6a8765e8721bb24a5cb65333e194f36b232990c0
Claire-Kimbugwe <31630227+Claire-Kimbugwe@users.noreply.github.com>
2017-09-15 12:18:26 -0700

    Update Test3.R

--- Test3.R
+++ Test3.R
@@ -1,1 +1,14 @@
+# Qn 1
+
+
+# Q4.
+#load Mtcars from R datasets 
+#Continuing with the 'mtcars' dataset from the previous Question, what is the absolute difference between the average horsepower
+#of 4-cylinder cars and the average horsepower of 8-cylinder cars?
+
+library(datasets)
+data("mtcars')
+> round(mean(mtcars[mtcars$cyl == "8",]$hp) - mean(mtcars[mtcars$cyl == "4",]$hp))
+[1] 127
+Q5.
 
